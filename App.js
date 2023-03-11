8815b214a3d75596644ab0ece0dfa1bd484667fa
Marcin Majewski <smdesign.eu@gmail.com>
2023-03-11 14:34:07 +0100

    single Message component (cnditional styling), ChatScreen with FlatList)

--- App.js
+++ App.js
@@ -1,28 +1,16 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
 // import ChatListItem from './src/components/ChatListItem'
-import ChatList from './src/screens/ChatList'
-
-// chat data static for now
-// const chat = {
-//   id: '1',
-//   user: {
-//     image:
-//       'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
-//     name: 'Lukas',
-//   },
-//   lastMessage: {
-//     text: 'Oke',
-//     createdAt: '07:30',
-//   },
-// }
+// import ChatList from './src/screens/ChatList'
+import ChatScreen from './src/screens/ChatScreen'
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ChatListItem chat={chat} />
       <ChatListItem chat={chat} /> */}
-      <ChatList />
+      {/* <ChatList /> */}
+      <ChatScreen />
 
       <StatusBar style="auto" />
     </View>
---
+++ src/components/Message/index.js
@@ -0,0 +1,40 @@
+import { View, Text, StyleSheet } from 'react-native'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
+
+const Message = ({ msg }) => {
+  const checkMyMassage = () => {
+    return msg.user.id === 'u1'
+  }
+
+  return (
+    <View
+      style={[
+        styles.container,
+        {
+          backgroundColor: checkMyMassage(msg) ? '#eeffdd' : 'white',
+          alignSelf: checkMyMassage(msg) ? 'flex-end' : 'flex-start',
+        },
+      ]}
+    >
+      <Text>{msg.text}</Text>
+      <Text style={styles.time}>{dayjs(msg.createdAt).fromNow(true)}</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 5,
+    padding: 10,
+    borderRadius: 5,
+    maxWidth: '80%',
+  },
+  time: {
+    color: 'gray',
+    alignSelf: 'flex-end',
+  },
+})
+
+export default Message
